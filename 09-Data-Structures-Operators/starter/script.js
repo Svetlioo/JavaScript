1489ea625ccfb01b312e78bb8619a4207272ed3d
Svetoslav Iliev <svetlio.iliev17@gmail.com>
2023-04-18 20:35:17 +0300

    Learned destructuring and ... operator

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -12,6 +12,9 @@ const restaurant = {
   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
 
+  order: function (starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+  },
   openingHours: {
     thu: {
       open: 12,
@@ -26,4 +29,122 @@ const restaurant = {
       close: 24,
     },
   },
+  orderDelivery: function (obj) {
+    console.log(obj);
+  },
+  orderPasta: function (ing1, ing2, ing3) {
+    console.log(`${ing1}, ${ing2}, ${ing3} `);
+  },
 };
+
+
+
+
+
+
+
+
+
+
+
+// const mainMenu = [...restaurant.mainMenu];
+
+// const menu = [...restaurant.mainMenu, ...restaurant.starterMenu];
+
+// console.log(menu);
+// const str = 'Djami';
+// const letters = [...str, '', 'S.'];
+// console.log(...letters);
+
+// const ingredient = [prompt('Ingridient 1'),prompt('Ingridient 2'),prompt('Ingridient 3')]
+
+
+
+// const newRestaurant = {
+//   founded: 1998,
+//   ...restaurant,
+//   founder: 'djami',
+// };
+
+// console.log(newRestaurant);
+
+// const newMenu = [...restaurant.mainMenu, 'Gnocci'];
+// console.log(...newMenu);
+
+// const arr = [7, 8, 9];
+
+// const badNewArr = [1, 2, arr[0], arr[1], arr[2]];
+// console.log(badNewArr);
+
+// const newArr = [1, 2, ...arr];
+// console.log(newArr);
+
+// console.log(...newArr);
+
+// const { name, openingHours, categories } = restaurant;
+// console.log(name, openingHours, categories);
+
+// const {
+//   name: restaurantName,
+//   openingHours: hours,
+//   categories: tags,
+// } = restaurant;
+// console.log(restaurantName, hours, tags);
+
+// const { menu = [], starterMenu: starters = [] } = restaurant;
+
+// console.log(menu, starters);
+
+// let a = 111;
+// let b = 999;
+// const obj = { a: 23, b: 7, c: 14 };
+
+// ({ a, b } = obj);
+
+// console.log(a, b);
+
+// const {
+//   fri: { open: o, close: c },
+// } = openingHours;
+// console.log(o, c);
+
+// restaurant.orderDelivery({
+//   time: '22:30',
+//   address: 'Via del Sole,21',
+//   mainIndex: 2,
+//   starterIndex: 2,
+// });
+
+// const arr = [2, 3, 4];
+// const a = arr[0];
+// const b = arr[1];
+// const c = arr[2];
+
+// const [x, y, z] = arr;
+// console.log(x, y, z);
+// console.log(arr);
+
+// let [main, , secondary] = restaurant.categories;
+// console.log(main, secondary);
+
+// const temp = main;
+// main = secondary;
+// secondary = temp;
+// console.log(main, secondary);
+
+// [main, secondary] = [secondary, main];
+// console.log(main, secondary);
+
+// const [starter, mainCourse] = restaurant.order(2, 0);
+// console.log(starter, mainCourse);
+
+// const nested = [2, 4, [5, 6]];
+
+// const [i, ,j] = nested;
+// // console.log(i,j);
+
+// const [i, , [j, k]] = nested;
+// console.log(i, j, k);
+
+// const [p=1, q=1, r=1] = [8];
+// console.log(p, q, r);
