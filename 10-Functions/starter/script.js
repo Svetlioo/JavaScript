cc686ac0e353f342dfbee72a03eaeb744bc362aa
Svetoslav Iliev <svetlio.iliev17@gmail.com>
2023-06-05 20:26:14 +0300

    learning

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -1,116 +1,197 @@
 'use strict';
 
-const lufthansa = {
-  airline: 'Lufthansa',
-  iataCode: 'LH',
-  bookings: [],
-  book(flightNum, name) {
-    console.log(
-      `${name} booked a seat on ${this.airline} flight ${this.iataCode}${flightNum}`
-    );
-    this.bookings.push({ flight: `${this.iataCode}${flightNum}`, name });
-  },
+const secureBooking = function () {
+  let passengerCount = 0;
+  return function () {
+    passengerCount++;
+    console.log(`${passengerCount} passengers`);
+  };
 };
 
-lufthansa.book(239, 'Djamistyle');
-lufthansa.book(635, 'Dgeto');
+const booker = secureBooking();
 
-const eurowings = {
-  name: 'Eurowings',
-  iataCode: 'EW',
-  bookings: [],
-};
+booker();
+booker();
+booker();
 
-const book = lufthansa.book;
 
-book.call(eurowings, 23, 'Sarah Williams');
-console.log(eurowings);
+// Coding Challenge #1
 
-book.call(lufthansa, 239, 'Mary Cooper');
+/* 
+Let's build a simple poll app!
 
-const swiss = {
-  airline: 'Swiss Air Lines',
-  iataCode: 'LX',
-  bookings: [],
-};
+A poll has a question, an array of options from which people can choose, and an array with the number of replies for each option. This data is stored in the starter object below.
 
-book.call(swiss, 583, 'Mary');
-console.log(swiss);
+Here are your tasks:
 
-const flightData = [583, 'George Cooper'];
-book.call(swiss, ...flightData);
+1. Create a method called 'registerNewAnswer' on the 'poll' object. The method does 2 things:
+  1.1. Display a prompt window for the user to input the number of the selected option. The prompt should look like this:
+        What is your favourite programming language?
+        0: JavaScript
+        1: Python
+        2: Rust
+        3: C++
+        (Write option number)
+  
+  1.2. Based on the input number, update the answers array. For example, if the option is 3, increase the value AT POSITION 3 of the array by 1. Make sure to check if the input is a number and if the number makes sense (e.g answer 52 wouldn't make sense, right?)
+2. Call this method whenever the user clicks the "Answer poll" button.
+3. Create a method 'displayResults' which displays the poll results. The method takes a string as an input (called 'type'), which can be either 'string' or 'array'. If type is 'array', simply display the results array as it is, using console.log(). This should be the default option. If type is 'string', display a string like "Poll results are 13, 2, 4, 1". 
+4. Run the 'displayResults' method at the end of each 'registerNewAnswer' method call.
 
-// const greet = function (greeting) {
-//   return function (name) {
-//     console.log(`${greeting} ${name}`);
-//   };
+HINT: Use many of the tools you learned about in this and the last section 😉
+
+BONUS: Use the 'displayResults' method to display the 2 arrays in the test data. Use both the 'array' and the 'string' option. Do NOT put the arrays in the poll object! So what shoud the this keyword look like in this situation?
+
+BONUS TEST DATA 1: [5, 2, 3]
+BONUS TEST DATA 2: [1, 5, 3, 9, 6, 1]
+
+GOOD LUCK 😀
+*/
+
+// const poll = {
+//   question: 'What is your favourite programming language?',
+//   options: ['0: JavaScript', '1: Python', '2: Rust', '3: C++'],
+//   // This generates [0, 0, 0, 0]. More in the next section 😃
+//   answers: new Array(4).fill(0),
+//   registerNewAnswer() {
+//     const answer = Number(
+//       prompt(
+//         `${this.question}\n${this.options.join('\n')}\n(Write option number)`
+//       )
+//     );
+//     typeof answer === 'number' &&
+//       answer < this.options.length &&
+//       this.answers[answer]++;
+
+//     this.displayResults();
+//     this.displayResults('string');
+//   },
+//   displayResults(type = 'array') {
+//     if (type === 'array') {
+//       console.log(this.answers);
+//     } else if (type === 'string') {
+//       console.log(`Poll results are ${this.answers.join(', ')}`);
+//     }
+//   },
 // };
 
-const greet = greeting => name => console.log(`${greeting} ${name}`);
+// document
+//   .querySelector('.poll')
+//   .addEventListener('click', poll.registerNewAnswer.bind(poll));
 
-const greeterHey = greet('Zdr');
+// poll.displayResults.call({ answers: [5, 2, 3] }, 'string');
 
-greeterHey('Jonas');
-greeterHey('Steven');
+// const lufthansa = {
+//   airline: 'Lufthansa',
+//   iataCode: 'LH',
+//   bookings: [],
+//   book(flightNum, name) {
+//     console.log(
+//       `${name} booked a seat on ${this.airline} flight ${this.iataCode}${flightNum}`
+//     );
+//     this.bookings.push({ flight: `${this.iataCode}${flightNum}`, name });
+//   },
+// };
 
-greet('Hello')('Jonas');
+// lufthansa.book(239, 'Djamistyle');
+// lufthansa.book(635, 'Dgeto');
 
-const oneWord = function (str) {
-  return str.replace(/ /g, '').toLowerCase();
-};
+// const eurowings = {
+//   name: 'Eurowings',
+//   iataCode: 'EW',
+//   bookings: [],
+// };
 
-const upperFirstWord = function (str) {
-  const [first, ...other] = str.split(' ');
-  return [first.toUpperCase(), ...other].join(' ');
-};
+// const book = lufthansa.book;
 
-const transformer = function (str, fn) {
-  console.log(`Original string: ${str}`);
-  console.log(`Transformed string: ${fn(str)}`);
+// book.call(eurowings, 23, 'Sarah Williams');
+// console.log(eurowings);
 
-  console.log(`Transformed by: ${fn.name}`);
-};
+// book.call(lufthansa, 239, 'Mary Cooper');
 
-transformer('Javascript is the best', upperFirstWord);
-transformer('Javascript is the best', oneWord);
+// const swiss = {
+//   airline: 'Swiss Air Lines',
+//   iataCode: 'LX',
+//   bookings: [],
+// };
 
-const high5 = function () {
-  console.log('uq v boba');
-};
-document.body.addEventListener('click', high5);
+// book.call(swiss, 583, 'Mary');
+// console.log(swiss);
 
-['Jonas', 'Martha', 'Adam'].forEach(high5);
-const bookings = [];
+// const flightData = [583, 'George Cooper'];
+// book.call(swiss, ...flightData);
 
-const createBooking = function (flightNum, numPassengers = 1, price = 199) {
-  //   numPassengers = numPassengers || 1;
-  //   price = price || 199;
+// // const greet = function (greeting) {
+// //   return function (name) {
+// //     console.log(`${greeting} ${name}`);
+// //   };
+// // };
 
-  const booking = {
-    flightNum,
-    numPassengers,
-    price,
-  };
-  console.log(booking);
-  bookings.push(booking);
-};
+// const greet = greeting => name => console.log(`${greeting} ${name}`);
 
-createBooking('LH123');
+// const greeterHey = greet('Zdr');
 
-const flight = 'LH234';
-const jonas = {
-  name: 'Jonas Schmedtmann',
-  passport: 23123123123,
-};
+// greeterHey('Jonas');
+// greeterHey('Steven');
 
-const checkIn = function (flightNum, passenger) {
-  flightNum = 'LH999';
-  passenger.name = 'Mr.' + passenger.name;
-  if (passenger.passport === 23123123123) {
-    alert('Checked in');
-  } else {
-    alert('Wrong!');
-  }
-};
+// greet('Hello')('Jonas');
 
-checkIn(flight,jonas);
+// const oneWord = function (str) {
+//   return str.replace(/ /g, '').toLowerCase();
+// };
+
+// const upperFirstWord = function (str) {
+//   const [first, ...other] = str.split(' ');
+//   return [first.toUpperCase(), ...other].join(' ');
+// };
+
+// const transformer = function (str, fn) {
+//   console.log(`Original string: ${str}`);
+//   console.log(`Transformed string: ${fn(str)}`);
+
+//   console.log(`Transformed by: ${fn.name}`);
+// };
+
+// transformer('Javascript is the best', upperFirstWord);
+// transformer('Javascript is the best', oneWord);
+
+// const high5 = function () {
+//   console.log('dge');
+// };
+// document.body.addEventListener('click', high5);
+
+// ['Jonas', 'Martha', 'Adam'].forEach(high5);
+// const bookings = [];
+
+// const createBooking = function (flightNum, numPassengers = 1, price = 199) {
+//   //   numPassengers = numPassengers || 1;
+//   //   price = price || 199;
+
+//   const booking = {
+//     flightNum,
+//     numPassengers,
+//     price,
+//   };
+//   console.log(booking);
+//   bookings.push(booking);
+// };
+
+// createBooking('LH123');
+
+// const flight = 'LH234';
+// const jonas = {
+//   name: 'Jonas Schmedtmann',
+//   passport: 23123123123,
+// };
+
+// const checkIn = function (flightNum, passenger) {
+//   flightNum = 'LH999';
+//   passenger.name = 'Mr.' + passenger.name;
+//   if (passenger.passport === 23123123123) {
+//     console.log('Checked in');
+//   } else {
+//     console.log('Wrong!');
+//   }
+// };
+
+// checkIn(flight, jonas);
