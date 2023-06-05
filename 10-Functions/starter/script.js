f22b93674b9a68b844c7b4fb6c0e88e8a935bbac
Svetoslav Iliev <svetlio.iliev17@gmail.com>
2023-06-05 20:36:38 +0300

    learning

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -176,71 +176,71 @@ GOOD LUCK 😀
 //   bookings.push(booking);
 // };
 
-const greet = greeting => name => console.log(`${greeting} ${name}`);
+// const greet = greeting => name => console.log(`${greeting} ${name}`);
 
-const greeterHey = greet('Zdr');
+// const greeterHey = greet('Zdr');
 
-greeterHey('Jonas');
-greeterHey('Steven');
+// greeterHey('Jonas');
+// greeterHey('Steven');
 
-greet('Hello')('Jonas');
+// greet('Hello')('Jonas');
 
-const oneWord = function (str) {
-  return str.replace(/ /g, '').toLowerCase();
-};
+// const oneWord = function (str) {
+//   return str.replace(/ /g, '').toLowerCase();
+// };
 
-const upperFirstWord = function (str) {
-  const [first, ...other] = str.split(' ');
-  return [first.toUpperCase(), ...other].join(' ');
-};
+// const upperFirstWord = function (str) {
+//   const [first, ...other] = str.split(' ');
+//   return [first.toUpperCase(), ...other].join(' ');
+// };
 
-const transformer = function (str, fn) {
-  console.log(`Original string: ${str}`);
-  console.log(`Transformed string: ${fn(str)}`);
+// const transformer = function (str, fn) {
+//   console.log(`Original string: ${str}`);
+//   console.log(`Transformed string: ${fn(str)}`);
 
-  console.log(`Transformed by: ${fn.name}`);
-};
+//   console.log(`Transformed by: ${fn.name}`);
+// };
 
-transformer('Javascript is the best', upperFirstWord);
-transformer('Javascript is the best', oneWord);
+// transformer('Javascript is the best', upperFirstWord);
+// transformer('Javascript is the best', oneWord);
 
-const high5 = function () {
-  console.log('uq v boba');
-};
-document.body.addEventListener('click', high5);
+// const high5 = function () {
+//   console.log('uq v boba');
+// };
+// document.body.addEventListener('click', high5);
 
-['Jonas', 'Martha', 'Adam'].forEach(high5);
-const bookings = [];
+// ['Jonas', 'Martha', 'Adam'].forEach(high5);
+// const bookings = [];
 
-const createBooking = function (flightNum, numPassengers = 1, price = 199) {
+// const createBooking = function (flightNum, numPassengers = 1, price = 199) {
   //   numPassengers = numPassengers || 1;
   //   price = price || 199;
 
-  const booking = {
-    flightNum,
-    numPassengers,
-    price,
-  };
-  console.log(booking);
-  bookings.push(booking);
-};
+//   const booking = {
+//     flightNum,
+//     numPassengers,
+//     price,
+//   };
+//   console.log(booking);
+//   bookings.push(booking);
+// };
 
-createBooking('LH123');
+// createBooking('LH123');
 
-const flight = 'LH234';
-const jonas = {
-  name: 'Jonas Schmedtmann',
-  passport: 23123123123,
-};
+// const flight = 'LH234';
+// const jonas = {
+//   name: 'Jonas Schmedtmann',
+//   passport: 23123123123,
+// };
 
-const checkIn = function (flightNum, passenger) {
-  flightNum = 'LH999';
-  passenger.name = 'Mr.' + passenger.name;
-  if (passenger.passport === 23123123123) {
-    alert('Checked in');
-  } else {
-    alert('Wrong!');
-  }
-};
+// const checkIn = function (flightNum, passenger) {
+//   flightNum = 'LH999';
+//   passenger.name = 'Mr.' + passenger.name;
+//   if (passenger.passport === 23123123123) {
+//     alert('Checked in');
+//   } else {
+//     alert('Wrong!');
+//   }
+// };
 
-checkIn(flight,jonas);
+// checkIn(flight,jonas);
